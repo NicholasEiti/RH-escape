23dca4bb7456c111b8841d14fcf6a79085e583a9
Nicholas Eiti Dan <eiti@eiti-debian.com>
2023-06-21 11:34:14 -0300

    Setup teste de animação completo

--- script.js
+++ script.js
@@ -1,35 +1,50 @@
 // Estou me baseando no vídeo: https://youtu.be/GFO_txvwK_c
 
+document.getElementById("animations").addEventListener("change" ,(e) => {
+    playerState = e.target.value
+});
+
 const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
+
+const playerImage = new Image();
 
 const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 
-const playerImage = new Image();
-playerImage.src = "Red hood free (zipped) Folder/idle and  alter/idle sheet-Sheet.png"
+let playerState = "idle"
 
 ctx.imageSmoothingEnabled = false;
 
-let f = 0;
-let delay = 0;
+let gameFrame = 0;
+const staggerFrames = 4;
+
+const animationStates = {
+    idle:{
+        sheetpath: "Red hood spritesheets/idle and  alter/idle sheet-Sheet.png",
+        frames: 18,
+        width: 80,
+        height: 80
+    },
+    run:{
+        sheetpath: "Red hood spritesheets/idle and  alter/itch run-Sheet sheet.png",
+        frames: 24,
+        width: 80,
+        height: 80
+    }
+}
+
+console.log(animationStates);
 
 function animate(){
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     // ctx.fillRect(50,50,100,100);
-    ctx.drawImage(playerImage, f*80, 0, 80, 80, 0, 0, 80*5, 80*5);
+    let state = animationStates[playerState];
+    playerImage.src = state.sheetpath;
+    let position = Math.floor(gameFrame/staggerFrames) % state.frames;
+    ctx.drawImage(playerImage, position*state.width, 0, state.width, state.height, 0, 0, 600, 600);
     requestAnimationFrame(animate);
-    if(delay === 0){
-        delay++;
-        f++;
-    }
-    else{
-        delay++;
-        if(delay >= 5) delay = 0;
-    }
-    if(f > 17){
-        f = 0;
-    }
+    gameFrame++;
 };
 
 animate();
